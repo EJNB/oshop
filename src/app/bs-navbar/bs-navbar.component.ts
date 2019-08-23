09d73006b1395f6cca5af8b12a8a8550b5cbc1b0
Javier <javiernieto1989@gmail.com>
2019-08-22 20:58:55 -0400

    Generate components admin/admin-orders, products, order-success and so on

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styles: []
+})
+export class BsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
